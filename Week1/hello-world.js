23f75201e3b030fbf1b977810abd86fc0efff8b1
Michelle Nesbitt <mn218@nerdymail.com>
2020-02-15 11:25:14 -0500

    1.3

--- Week1/hello-world.js
+++ Week1/hello-world.js
@@ -1,1 +1,13 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Michelle Nesbitt
+; Date: 15 February 2020
+; Modified By: Michelle Nesbitt
+; Description: This program demonstrates the
+; use of errors in JavaScript types, values, and
+; and variables in an application.
+;===========================================
+*/ 
+
 console.log("Hello World, my name is Michelle");
