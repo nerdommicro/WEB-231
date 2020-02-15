6b693f5fe9527b7ea68c976198d6b1b26430e5c0
Michelle Nesbitt <mn218@nerdymail.com>
2020-02-15 11:31:13 -0500

    1131am

--- Week1/Nesbitt-Assignment1-4.js
+++ Week1/Nesbitt-Assignment1-4.js
@@ -1,12 +1,16 @@
 /*
 ============================================
-; Title: Assignment 1.3
+; Title: Assignment 1.4
 ; Author: Michelle Nesbitt
 ; Date: 15 February 2020
 ; Modified By: Michelle Nesbitt
 ; Description: This program demonstrates the
-; use a simple console.log statement.
+; use of variables.
 ;===========================================
 */ 
-
-console.log("Hello World, my name is Michelle");
+var firstName = "Michelle";
+var lastName = "Nesbitt";
+var age = "21";
+var street = "100 Main St";
+var city = "Levittown";
+var zipCode = "11756";
