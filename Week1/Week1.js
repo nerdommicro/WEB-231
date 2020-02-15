6c293735581a4861bae051af4830fe246d740136
Michelle Nesbitt <mn218@nerdymail.com>
2020-02-15 09:16:53 -0500

    discussion

---
+++ Week1/Week1.js
@@ -0,0 +1,20 @@
+/*
+============================================
+; Title: Discussion 1.1
+; Author: Michelle Nesbitt
+; Date: 14 February 2020
+; Modified By: Michelle Nesbitt
+; Description: This program demonstrates the
+; use of errors in JavaScript types, values, and
+; and variables in an application.
+;===========================================
+*/ 
+const myVariable = 5;
+myVariable = 6;
+
+var sayHelloToMe = "Hello 
+Michelle";
+
+var 5sum = myVariable + sayHelloToMe;
+
+console.log(5sum);
