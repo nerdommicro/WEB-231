4dbe79e25b90926e9d439dba2f2c93104189e1d7
michellenesbitt <mn218@nerdymail.com>
2020-02-25 07:11:27 -0500

    week2

---
+++ Week 2/header-demo.js
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title:  header-demo.js
+; Author: Professor Krasso
+; Date:   23 February 2020
+; Modified By: Michelle Nesbitt
+; Description: Displays my formatted header
+;===========================================
+*/
+
+/**
+* Params: firstName, lastName, assignment
+* Response: output
+* Description: Returns my formatted header
+*/
+
+const header = require('./Nesbitt-header');
+
+console.log(header.display("Michelle", "Nesbitt", "Week 2"));
